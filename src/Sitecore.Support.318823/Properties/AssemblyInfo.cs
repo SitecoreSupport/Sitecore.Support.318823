92dca5abef688f3100fdf15d1996fb5a0b1d042a
DK\tpe <tpe@sitecore.net>
2019-03-22 15:35:12 +0200

    Initial commit

---
+++ src/Sitecore.Support.318823/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.318823")]
+[assembly: AssemblyProduct("Sitecore.Support.318823")]
+[assembly: ComVisible(false)]
